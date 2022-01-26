96f5c2255cfa40a759ee13cde25bed34c4f46e4d
EasierByCode[43] <daniel@community43.org>
2022-01-26 09:50:28 -0700

    move endpoint functionality into page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,44 @@
     import base from '$lib/base.js'
 
     let days
+
+    const post = async ({ params, request }) => {
+        const body = request.formData;
+        const name = body.get("name");
+        const days = body.getAll("days");
+        let fetchUrl = `https://docs.google.com/forms/d/e/1FAIpQLSdDw9nf3zt8ebxN9kZLIYLAGGmmFcSZrT7_FoHzfiYhpm3ehw/formResponse?usp=pp_url&entry.391768003=${name}`;
+        
+        if (days.includes('Thursday')) fetchUrl += '&entry.1314416783=Thursday';
+        if (days.includes('Friday')) fetchUrl += '&entry.1314416783=Friday';
+        if (days.includes('Saturday')) fetchUrl += '&entry.1314416783=Saturday';
+
+        fetchUrl += '&submit=Submit'
+
+        const res = await fetch(fetchUrl, { mode: 'no-cors' });
+
+        if (res.status === 200) {
+            return {
+                status: 200,
+                body: {
+                    days
+                }
+            }
+        } else {
+            return {
+                status: 404,
+                body: {
+                    message: "failed"
+                }
+            }
+        }
+    }
     
     const submitForm = async (data) => {
         const formData = new FormData(data.currentTarget)
         days = formData.getAll('days')
-        const res = await fetch("registrant.json", {
-            method: "POST",
-            body: formData,
+        const res = await post({
+            params: {},
+            request: { formData },
         })
     }
 </script>
